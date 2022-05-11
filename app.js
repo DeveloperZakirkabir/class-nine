9b831e65197459b0f91c060800c320bd8eca33a5
MOHAMMAD ZAKIR HOSSAIN <89220048+DeveloperZakirkabir@users.noreply.github.com>
2022-05-11 23:38:01 +0600

    Add files via upload

---
+++ app.js
@@ -0,0 +1,33 @@
+
+const devs=[
+    { 
+        id  : 1,
+        name : 'Zakir ',
+        age :32 ,
+        skill :'wordpress customoizar '
+
+    },
+    { 
+        id  : 2,
+        name : 'kabir ',
+        age :32 ,
+        skill :'wordpress customoizar ',
+
+    },
+    { 
+        id  : 3, 
+        name : 'hossain ',
+        age :32 ,
+        skill :'wordpress cutomoizar ',
+
+    },
+    { 
+        id  : 4,
+        name : 'gazi ',
+        age :32 ,
+        skill :'html customoizar '
+    },
+    
+]
+
+console.log(devs);
